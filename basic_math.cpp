0cd817487a509cdfe6bf8634ba775196a6a6b1a8
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-02-12 18:11:29 +0000

    ye

---
+++ basic_math.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2025 Jack Turcotte All rights reserved.
+//
+// Created by: Jack Turcotte
+// Date: February 12, 2025
+// This program performs basic math operations
+
+#include <iostream>
+#include <cmath>  // import the BEST math library (math.h is not updated as much as cmath)
+
+int main() {
+    std::cout << "9 + 2 = " << 9+2 << std::endl;
+    std::cout << "5 - 7 = " << 5-7 << std::endl;
+    std::cout << "16 * 2 = " << 16*2 << std::endl;
+    std::cout << "121 / 11 = " << 121/11 << std::endl;
+    std::cout << "3 ** 2 = " << pow(3, 2) << std::endl;
+    std::cout << "root 225 = " << sqrt(225) << std::endl;
+}
